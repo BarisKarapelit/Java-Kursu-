d52ea14e503735f5dfb1782f9f12c66e04e35a31
Barış Karapelit <baris_karapelit_298@hotmail.com>
2020-07-29 20:45:00 +0300

    Method Nene Gönderme

---
+++ MethodlaraNesneGonderme/src/com/baris/Main.java
@@ -0,0 +1,15 @@
+package com.baris;
+
+import static java.lang.Integer.sum;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int a=15;
+        int b=20;
+        int sum=sum(a,b);
+        System.out.println(sum);
+    }
+}
